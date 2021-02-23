3ff4ab7ec5511aa6162395d89edb3bdfd63df3b5
SHIMA Tatsurou <shima.tatsurou@gmail.com>
2021-02-23 17:49:12 +0900

    bug fix in update method, add comments to each method

--- drink.rb
+++ drink.rb
@@ -8,7 +8,7 @@ class Drink
     WATER=3
   end
 
-  #以下は隠蔽
+  # 価格表（隠蔽）
   ITEMS_MASTER={
     Kind::COLA => {name: "cola", price: 120},
     Kind::REDBULL => {name: "redbull", price: 200},
@@ -17,10 +17,12 @@ class Drink
   private_constant :ITEMS_MASTER
 
   # インスタンス生成, 初期化
-  # 使用例1
-  # v = Drink.new(4, "miku", 200)
-  # 使用例2, 価格表に登録済みの飲み物は種別指定のみでok
-  # v = Drink.new(1)
+  # 使用例1, 価格表に登録済みの飲み物は種別指定のみでOK
+  #   v = Drink.new(Kind::COLA)
+  # 使用例2
+  #   v = Drink.new(4, "milk", 200)
+  # 注意, 指定された種別が数値で無い場合は例外をスローする
+  #   v = Drink.new("cola") --> DrinkInitializ
   def initialize(kind, name='', price=0)
     if(kind.class!=Integer)
       raise DrinkInitializeError, "種別は正の整数値を指定してください. #{kind}.to_s"
@@ -37,23 +39,26 @@ class Drink
   end
 
   # 以下はインスタンスメソッド定義
-  # 使用例
-  # d = Drink.new(1); d.kind
+
+  # 種別を返す
   def kind
     @kind
   end
-  
+
+  # 名前を返す
   def name
     @name
   end
 
+  # 価格を返す
   def price
     @price
   end
 
   # 以下はクラスメソッド定義
+  # 価格表から、指定した飲み物の価格を取得  
   # 使用例
-  # price = Drink::price(1)
+  # price = Drink::price(Kind::COLA)
   def self.price (kind)
     if(kind.class!=Integer)
       puts '種別は正の整数値を指定してください'   #return nil
@@ -64,6 +69,9 @@ class Drink
     end
   end
 
+  # 価格表から、指定した飲み物の名前を取得
+  # 使用例
+  # name =  Drink::name(Kind::COLA)
   def self.name (kind)
     if(kind.class!=Integer)
       puts '種別は正の整数値を指定してください'   #return nil
@@ -74,6 +82,9 @@ class Drink
     end
   end
 
+  # 価格表に、指定した飲み物の名前/価格を追加
+  # 使用例
+  # result =  Drink::insert(4, "milk", 200)
   def self.insert(kind, name, price)
     if(kind.class!=Integer)
       puts '種別は正の整数値を指定してください'   #return nil
@@ -84,13 +95,17 @@ class Drink
     end
   end
 
+  # 価格表の、指定した飲み物の名前/価格を更新
+  # 使用例
+  # result =  Drink::update(Kind::COLA, "coke", 150)
   def self.update(kind, name: name="Unspecified", price: price=-1)
     if(kind.class!=Integer)
       puts '種別は正の整数値を指定してください'   #return nil
     elsif ITEMS_MASTER.has_key?(kind)
-      if (name!="Unspecified" && name.empty? )
+      if ( (name != "Unspecified")  && !name.empty? )
         ITEMS_MASTER[kind][:name]=name
-      elsif (price >= 0)
+      end
+      if (price >= 0)
         ITEMS_MASTER[kind][:price]=price
       end
       ITEMS_MASTER[kind]
@@ -99,6 +114,9 @@ class Drink
     end
   end
 
+  # 価格表の、指定した飲み物の名前/価格を削除
+  # 使用例
+  # result = Drink::destroy(Kind::COLA)
   def self.destroy(kind)
     if(kind.class!=Integer)
       puts '種別は正の整数値を指定してください'   #return nil
@@ -107,6 +125,9 @@ class Drink
     end
   end
 
+  # 価格表の内容を文字列で出力
+  # 使用例
+  # result = Drink::master
   def self.master
     ITEMS_MASTER.each do |kind, item|
       puts "種別:#{kind.to_s}, 名前:#{item[:name]}, 価格:#{item[:price]}\n"
